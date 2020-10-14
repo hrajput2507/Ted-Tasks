2f55933b672236cee9633ef8005cf8c3e0314ef2
himanshu rajput <hrajput2507@gmail.com>
2020-10-14 09:34:28 +0530

    comment part done

--- backend/models/users.js
+++ backend/models/users.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 
-const UserSchema = new Schema({
-    
+const UserSchema = new Schema({  
+     
     _id: Schema.Types.ObjectId,
     name : String,
     email : String,
